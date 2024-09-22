cbf07fb3c6272bdee494abb77e738357233a91a5
Steven Morse <steven.t.morse@gmail.com>
2024-09-22 08:18:07 -0400

    try 2

--- project/script.py
+++ project/script.py
@@ -1,9 +1,8 @@
 import requests
 import json
-import logging
 
 response = requests.get('https://api.github.com/events')
 event = response.json()[0]
 
-logging.warning(f'Last event: {event["type"]} | {event["repo"]["name"]}')
+print(f'Last event: {event["type"]} | {event["repo"]["name"]}', flush=True)
 

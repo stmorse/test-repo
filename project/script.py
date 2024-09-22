cc241dc0707977bfe62898db2ca56348f871fdfa
Steven Morse <steven.t.morse@gmail.com>
2024-09-22 07:38:51 -0400

    fix unbuffered

--- project/script.py
+++ project/script.py
@@ -1,9 +1,8 @@
 import requests
 import json
-import logging
 
 response = requests.get('https://api.github.com/events')
 event = response.json()[0]
 
-logging.info(f'Last event: {event['type']} | {event['repo']['name']}')
+print(f'Last event: {event['type']} | {event['repo']['name']}')
 

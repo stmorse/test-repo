94363adbd16fa3c386581d63b8f03992d5c4899e
Steven Morse <steven.t.morse@gmail.com>
2024-09-22 08:03:31 -0400

    fix quotes

--- project/script.py
+++ project/script.py
@@ -4,5 +4,6 @@ import json
 response = requests.get('https://api.github.com/events')
 event = response.json()[0]
 
-print(f'Last event: {event['type']} | {event['repo']['name']}')
+print('Logging...')
+print(f'Last event: {event["type"]} | {event["repo"]["name"]}')
 
